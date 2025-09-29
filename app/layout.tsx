ab049388bd8e0c1337ade13c6ef21fa8046845a2
Dzianis Minzakirau <denis@fideum.group>
2025-09-29 10:20:54 +0300

    Clarity - lp1

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru">
+      <head>
+        <script 
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "ti6rsv5fd8");
+            `
+          }}
+        />
+      </head>
       <body className="bg-gray-900 m-0 p-0">
         {children}
       </body>
